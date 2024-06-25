772ed5795cc9047eddbc53f0f0bc4e545a466c7e
Pavlo Svinarchuk <shokkontent2004@gmail.com>
2024-06-25 22:26:30 +0300

    make build

---
+++ index.eb71df00.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery"),r=e.querySelector("#largeImg");e.querySelector(".gallery__list").addEventListener("click",function(e){e.preventDefault();var t=e.target.closest(".list-item__link");if(t){var l=t.closest("a");r.src=l.href}});
+//# sourceMappingURL=index.eb71df00.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,14 @@
+"use strict";
+const gallery = document.querySelector(".gallery");
+const largeImg = gallery.querySelector("#largeImg");
+const smallImg = gallery.querySelector(".gallery__list");
+// eslint-disable-next-line no-shadow
+smallImg.addEventListener("click", (event)=>{
+    event.preventDefault();
+    const img = event.target.closest(".list-item__link");
+    if (!img) return;
+    const link = img.closest("a");
+    largeImg.src = link.href;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
